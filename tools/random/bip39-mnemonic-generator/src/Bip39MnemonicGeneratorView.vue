66c455ddaa2ddc20b86da3fbff1e9d9f079c0b6f
rwv <i@rwv.dev>
2026-01-13 16:27:14 +0800

    feat(tools): add BIP39 overview section

--- tools/random/bip39-mnemonic-generator/src/Bip39MnemonicGeneratorView.vue
+++ tools/random/bip39-mnemonic-generator/src/Bip39MnemonicGeneratorView.vue
@@ -1,6 +1,7 @@
 <template>
   <ToolDefaultPageLayout :info="toolInfo">
     <Bip39MnemonicGenerator />
+    <WhatIsBip39 />
   </ToolDefaultPageLayout>
 </template>
 
@@ -8,4 +9,5 @@
 import * as toolInfo from './info'
 import { ToolDefaultPageLayout } from '@shared/ui/tool'
 import Bip39MnemonicGenerator from './components/Bip39MnemonicGenerator.vue'
+import WhatIsBip39 from './components/WhatIsBip39.vue'
 </script>
