a1bbb5e9a3f0878a4a1b6a80acf486dc95554152
rwv <7891383+rwv@users.noreply.github.com>
2026-01-16 10:44:04 +0800

    feat(tools): add color picker tool (#163)
    * feat(tools): add color picker tool
    
    * fix(tools): address color picker type errors
    
    * feat(tools): allow svg uploads in color picker
    
    * feat(tools): add eyedropper icon
    
    * feat(tools): use image pick button
    
    * feat(tools): add drop overlay for color picker
    
    * fix(tools): make color picker drop overlay responsive
    
    * chore(tools): refresh color picker copy
    
    ---------
    
    Co-authored-by: zhongcan <hzc@debian-dev.intranet.home.zczc.host>

---
+++ tools/web/color-picker/src/ColorPickerView.vue
@@ -0,0 +1,11 @@
+<template>
+  <ToolDefaultPageLayout :info="toolInfo">
+    <ColorPickerTool />
+  </ToolDefaultPageLayout>
+</template>
+
+<script setup lang="ts">
+import * as toolInfo from './info'
+import { ToolDefaultPageLayout } from '@shared/ui/tool'
+import ColorPickerTool from './components/ColorPickerTool.vue'
+</script>
