fc424362685ef8b387cb90abc625935d6903785b
rwv <7891383+rwv@users.noreply.github.com>
2026-01-26 12:12:46 +0800

    feat(hash): add sha3 fips-202 hash tools (#221)
    * feat(hash): add sha3 fips-202 hash tools
    
    * fix(hash): address sha3 type checks
    
    * fix(hash): correct sha3 description

--- registry/tools/src/index.ts
+++ registry/tools/src/index.ts
@@ -26,6 +26,8 @@ import { toolInfo as networkToolsToolInfo } from '@tools/network-tools'
 import { toolInfo as pdfToolsToolInfo } from '@tools/pdf-tools'
 import { toolInfo as imageToolsToolInfo } from '@tools/image-tools'
 import { toolInfo as sha256HashTextOrFileToolInfo } from '@tools/sha256-hash-text-or-file'
+import { toolInfo as sha3_256HashTextOrFileToolInfo } from '@tools/sha3-256-hash-text-or-file'
+import { toolInfo as sha3_512HashTextOrFileToolInfo } from '@tools/sha3-512-hash-text-or-file'
 import { toolInfo as sha384HashTextOrFileToolInfo } from '@tools/sha384-hash-text-or-file'
 import { toolInfo as sha512HashTextOrFileToolInfo } from '@tools/sha512-hash-text-or-file'
 import { toolInfo as sha1HashTextOrFileToolInfo } from '@tools/sha1-hash-text-or-file'
@@ -213,6 +215,8 @@ export const tools: ToolInfo[] = [
 
   // Hash Tools
   sha256HashTextOrFileToolInfo,
+  sha3_256HashTextOrFileToolInfo,
+  sha3_512HashTextOrFileToolInfo,
   sha384HashTextOrFileToolInfo,
   sha512HashTextOrFileToolInfo,
   sha1HashTextOrFileToolInfo,
---
+++ tools/hash/sha3-512-hash-text-or-file/src/SHA3_512HashTextOrFileView.vue
@@ -0,0 +1,22 @@
+<template>
+  <ToolDefaultPageLayout :info="toolInfo">
+    <HashTextOrFileTemplate :hash="hashFunction" />
+    <WhatIsSHA3_512 />
+  </ToolDefaultPageLayout>
+</template>
+
+<script setup lang="ts">
+import * as toolInfo from './info'
+import { ToolDefaultPageLayout } from '@shared/ui/tool'
+import { HashTextOrFileTemplate } from '@tools/hash-text-or-file-template'
+import { sha3_512 } from '@noble/hashes/sha3'
+import WhatIsSHA3_512 from './WhatIsSHA3_512.vue'
+
+async function hashFunction(blob: Blob): Promise<ArrayBuffer> {
+  const arrayBuffer = await blob.arrayBuffer()
+  const hashBytes = sha3_512(new Uint8Array(arrayBuffer))
+  const buffer = new ArrayBuffer(hashBytes.byteLength)
+  new Uint8Array(buffer).set(hashBytes)
+  return buffer
+}
+</script>
