e354a970ab1fc225dd1168a51edb79169bcf1816
rwv <i@rwv.dev>
2026-02-28 13:56:18 +0800

    chore(test): remove unused vitest import

--- tools/favicon/favicon-assets-generator/src/components/general-info/GeneralInfo.dom.test.ts
+++ tools/favicon/favicon-assets-generator/src/components/general-info/GeneralInfo.dom.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { mount } from '@vue/test-utils'
 import type { GeneralInfoOptions } from '../../utils/favicon-generator/general-info'
 import GeneralInfo from './GeneralInfo.vue'
