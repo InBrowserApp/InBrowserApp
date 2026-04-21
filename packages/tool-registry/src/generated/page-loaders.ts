1f57ae39dc699854770b59cfa68c81a8d950b915
rwv <7891383+rwv@users.noreply.github.com>
2026-04-21 13:33:14 +0800

    feat(tool): migrate xxhash xxh3 64 hash text or file (#763)

--- packages/tool-registry/src/generated/page-loaders.ts
+++ packages/tool-registry/src/generated/page-loaders.ts
@@ -137,6 +137,8 @@ export const toolPageLoaders: Readonly<Record<string, ToolPageLoader>> = {
   "whirlpool-hash-text-or-file": () =>
     import("@tool/whirlpool-hash-text-or-file/page"),
   "xml-to-json-converter": () => import("@tool/xml-to-json-converter/page"),
+  "xxhash-xxh3-64-hash-text-or-file": () =>
+    import("@tool/xxhash-xxh3-64-hash-text-or-file/page"),
   "xxhash-xxh32-hash-text-or-file": () =>
     import("@tool/xxhash-xxh32-hash-text-or-file/page"),
   "xxhash-xxh64-hash-text-or-file": () =>
